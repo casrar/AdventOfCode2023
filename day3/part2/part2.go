1daaf38ec0f382e5b5bf1b4c3889b5cbd095b6c6
casrar <casey.j.morar@gmail.com>
2023-12-08 16:47:45 -0500

    finally handling the edge case

--- day3/part2/part2.go
+++ day3/part2/part2.go
@@ -23,7 +23,7 @@ func IsNumericChar(c byte) bool {
 }
 
 func main() {
-	raw_data, err := os.ReadFile("../test.txt")
+	raw_data, err := os.ReadFile("../input.txt")
 	if err != nil {
 		panic("Error reading file")
 	}
@@ -48,29 +48,20 @@ func main() {
 				continue
 			} 
 
-			fmt.Print("Offset: ")
-			fmt.Print(offset)
-			fmt.Println( " | curr: " + string(data[(i+offset)]))
-			
-
-			// unhandled edge cases
-			if first_index == -1 && IsNumericChar(data[(i+offset)]) {
-				fmt.Println("first if")
-				first_index = i + offset
+			if (offset == -1 || offset == 1) && IsNumericChar(data[(i+offset)]) {
+				is_continuous = false
 			} else if first_index > -1 && !IsNumericChar(data[(i+offset)]) {
-				fmt.Println("second if")
 				is_continuous = false
+			} 
+
+			if first_index == -1 && IsNumericChar(data[(i+offset)]) {
+				first_index = i + offset
 			} else if !is_continuous && IsNumericChar(data[(i+offset)]) {
-				fmt.Println("third if")
 				last_index = i + offset
-			} else if first_index > -1 && offset == 1 && IsNumericChar(data[(i+offset)]) { 
-				fmt.Println("fourth if")
-				is_continuous = false
-				last_index = i + offset
-			}
+			} 
+
 		}
 		if last_index == -1 || is_continuous {
-			fmt.Println("in")
 			continue
 		}
 		for {
@@ -116,7 +107,6 @@ func main() {
 			second_num += string(data[last_index])
 			last_index++
 		}
-		fmt.Println("First num: " + first_num + " | Second num: " + second_num)
 		sum += (StringToInteger(first_num) * StringToInteger(second_num))
 	}
 	fmt.Println(sum)
